cd266aa016216260b6c68fa3ac55db41deac95b0
Prince Boamah <princeboamah@Princes-MacBook-Air.local>
2020-04-03 15:27:01 +0000

    created lite app

---
+++ daylight/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'daylight.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url('', include('lite.urls', namespace="index")),
+]
